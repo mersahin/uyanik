382f1dee007aa4e8c3fb00a50d9b2c388b1d67d3
Mehmet Ersahin <ersahin.m@eplan.de>
2025-06-30 14:31:34 +0200

    .gitignore dosyasına /bin ve /obj dizinleri eklendi. Uyanik uygulamasına ait tüm derleme çıktıları ve geçici dosyalar silindi.

--- uyanik/obj/Debug/net6.0-windows/win-x64/Uyanik.AssemblyInfo.cs
+++ uyanik/obj/Debug/net6.0-windows/win-x64/Uyanik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uyanik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb5efd25e939cf2f82e5bb47e13390d134638b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uyanik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uyanik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
